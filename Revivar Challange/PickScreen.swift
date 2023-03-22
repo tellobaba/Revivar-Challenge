e2a48ae551fd13c68c35b6a06b769ff7ab602731
tellobaba <olatunjitello17@gmail.com>
2023-03-22 20:02:34 +0100

    Initial Commit + UI build

---
+++ Revivar Challange/PickScreen.swift
@@ -0,0 +1,16 @@
+//
+//  PickScreen.swift
+//  Revivar Challenge
+//
+//  Created by Oluwatomiwa on 22/03/2023.
+//
+
+import UIKit
+
+class PickScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
--- Revivar Challange/ViewController.swift
+++ Revivar Challange/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Revivar Challange
+//  Revivar Challenge
 //
 //  Created by Oluwatomiwa on 22/03/2023.
 //
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let selectButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpButton()
+        title = "Start"
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
+    func setUpButton() {
+        view.addSubview(selectButton)
+        
+        selectButton.configuration = .filled()
+        selectButton.configuration?.baseBackgroundColor = .black
+        selectButton.configuration?.title = "Start"
+        
+        selectButton.addTarget(self, action: #selector(goToNextScreen), for: .touchUpInside)
+        
+        selectButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            selectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            selectButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            selectButton.widthAnchor.constraint(equalToConstant: 200),
+            selectButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    @objc func goToNextScreen(){
+        let nextScreen = PickScreen()
+        nextScreen.title = "Select an Image"
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
 }
 
